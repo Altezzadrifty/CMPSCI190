af275470a37531d8368ce503ba12b3a08c24b0bf
Xander Turnbough <alexanderturnbough@Xanders-Macbook.attlocal.net>
2023-06-12 19:40:20 -0700

    lab 3

--- student.js
+++ student.js
@@ -10,3 +10,24 @@ var id2 = 931;
 function displayinfo991 (){
     alert ( thename + my_message + id2 );
 }
+
+// declare this variable outside of your functions
+current = 1;     // selects which item that is "on special"
+
+function displayspecial945() {
+
+   if (current == 1) {
+	  alert("The current special is #1");
+   }
+   if (current == 2)  {
+	  alert("The current special is #2");
+   }
+   if (current == 3 )  {
+	  alert("The current special is #3");
+   }
+   current = current + 1;
+   if (current > 3) {
+	 current = 1 ;  // wrap around
+   }
+
+}
