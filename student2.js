3f467de15b28fb2fcb59943ffa6517c19c1f0882
Xander Turnbough <alexanderturnbough@Xanders-Macbook.attlocal.net>
2023-06-26 17:44:29 -0700

    gloop 10

--- student2.js
+++ student2.js
@@ -1,6 +1,9 @@
 var companyname4 = "Xander's Clothes";
 var address3 = "251 Main St. Valencia, CA 91344";
 var phonenumber3 = "(555) 319-1234";
+var imgurl851 = "./Images/"
+var cardimgurl741 = "http://www.college1.com/images/cards/gbCard"
+
 function getHeader() {
     var headerHTML = "<header style='background-color: #ff6e4f; text-align: center; padding: 10px;'>"
                    + "<h1 style='font-size: 24px;'>" + companyname4 + "</h1>"
@@ -27,7 +30,9 @@ function getHeader() {
     for (const product of products) {
       menuHTML +=  `<button onClick='JavascriptFunction:execButton933(${JSON.stringify(product)})' > ${product.Name} </button>`;
     }
+     menuHTML += `<button onClick='JavascriptFunction:dealCards581()'> Deal Cards </button>`;
     return menuHTML;
+
   }
   function makeMain7(product) {
     var mainHTML = `My Product:<br/>`;
@@ -35,13 +40,23 @@ function getHeader() {
     mainHTML += `Product ID: ${product.id}<br/>`;
     mainHTML += `Product Description: ${product.desc}<br/>`;
     // mainHTML += `Shopping Cart Link Bar <br/>`; 
-    // mainHTML += `Product Image: <br/>`;
+    mainHTML += `Product Image: <img src="${imgurl851}${product.id}.jpg" /> `;
     return mainHTML;
   }
 
   function execButton933(product) {
     console.log(product)
     document.getElementById("Productdiv").innerHTML=makeMain7(product)
+  }
+
+  function dealCards581() {
+    var htmltoadd=""
+    htmltoadd +=`<img src="${cardimgurl741}${0}.gif" id="card0" />`
+    htmltoadd +=`<img src="${cardimgurl741}${1}.gif" id="card1" />`
+    htmltoadd +=`<img src="${cardimgurl741}${52}.gif" id="card2" />`
+    htmltoadd +=`<img src="${cardimgurl741}${52}.gif" id="card3" />`
+    htmltoadd +=`<img src="${cardimgurl741}${52}.gif" id="card4" />`
+    document.getElementById("Productdiv").innerHTML= htmltoadd
   }
 
 
